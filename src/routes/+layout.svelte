72510c31c9e635d15b5ed44fbe36fafa372f1859
Fabio Dessi <f.dessi@fabiodessi.dev>
2023-10-08 03:33:48 +0200

    feat: add header and footer placeholders

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,22 @@
-<svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Anton&family=Jost:wght@100;200;300;400;700;800;900&display=swap" rel="stylesheet">
-</svelte:head>
-
 <script>
-  import "../app.css";
-	import MenuOverlay from "../components/MenuOverlay/MenuOverlay.svelte";
+	import '../app.css';
+	import MenuOverlay from '../components/MenuOverlay/MenuOverlay.svelte';
+	import Header from '../components/Header/Header.svelte';
+	import Footer from '../components/Footer/Footer.svelte';
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Anton&family=Jost:wght@100;200;300;400;700;800;900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <MenuOverlay />
-<slot />  
+<div class="w-full px-8">
+	<Header />
+	<slot />
+	<Footer />
+</div>
