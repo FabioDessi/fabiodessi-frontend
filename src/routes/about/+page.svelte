c12f0939174a4a2e96d77231abe98c1eab8d5de6
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-02-18 20:17:18 +0100

    fix: add missing tags

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,64 +5,90 @@ import type { Section } from '../../types';
 
 const frontendSection: Section = {
   title: {
-    firstLine: "Frontend",
-    secondLine: "Experience",
+    firstLine: 'Frontend',
+    secondLine: 'Experience'
   },
   intro: {
-    title: "when branding takes on its full meaning",
-    content: "It’s no secret, we’ve always been more hoodie than suit-and-tie. More than ever, our new identity reflects who we really are. We took pleasure in turning our back on the corporate to embrace our irreverent style. <strong>After all, our cocky side is part of our personality.</strong>",
+    title: 'when branding takes on its full meaning',
+    content:
+    'It’s no secret, we’ve always been more hoodie than suit-and-tie. More than ever, our new identity reflects who we really are. We took pleasure in turning our back on the corporate to embrace our irreverent style. <strong>After all, our cocky side is part of our personality.</strong>'
   },
   blocks: [
-    { title: "Title 1",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
+    {
+      title: 'Title 1',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
     },
-    { title: "Title 2",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
+    {
+      title: 'Title 2',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
     },
-    { title: "Title 3",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
+    {
+      title: 'Title 3',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
     },
-    { title: "Title 4",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
+    {
+      title: 'Title 4',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
     },
-    { title: "Title 5",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
+    {
+      title: 'Title 5',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
     },
-    { title: "Title 6",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
+    {
+      title: 'Title 6',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
     },
-    { title: "Title 7",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
+    {
+      title: 'Title 7',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
     },
-    { title: "Title 8",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
-    },
-  ], 
+    {
+      title: 'Title 8',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
+    }
+  ]
 };
 
 const backendSection: Section = {
   title: {
-    firstLine: "Backend",
-    secondLine: "Experience",
+    firstLine: 'Backend',
+    secondLine: 'Experience'
   },
   intro: {
-    title: "when branding takes on its full meaning",
-    content: "It’s no secret, we’ve always been more hoodie than suit-and-tie. More than ever, our new identity reflects who we really are. We took pleasure in turning our back on the corporate to embrace our irreverent style. <strong>After all, our cocky side is part of our personality.</strong>",  
+    title: 'when branding takes on its full meaning',
+    content:
+    'It’s no secret, we’ve always been more hoodie than suit-and-tie. More than ever, our new identity reflects who we really are. We took pleasure in turning our back on the corporate to embrace our irreverent style. <strong>After all, our cocky side is part of our personality.</strong>'
   },
   blocks: [
-    { title: "Title 1",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
+    {
+      title: 'Title 1',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
     },
-    { title: "Title 2",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
+    {
+      title: 'Title 2',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
     },
-    { title: "Title 3",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
+    {
+      title: 'Title 3',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
     },
-    { title: "Title 4",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.", 
-    },
-  ],
+    {
+      title: 'Title 4',
+      content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum euismod nisl vel blandit. Aenean convallis augue sit amet lectus tempor, in molestie erat hendrerit.'
+    }
+  ]
 };
 </script>
 
@@ -75,11 +101,13 @@ const backendSection: Section = {
 <section class="flex items-center justify-center pb-[13rem]">
   <div class="container mx-auto px-16">
     <p class="font-robotoSlab text-5xl font-thin tracking-wide text-center leading-[4rem]">
-      Experienced Full Stack Engineer with 10 years of comprehensive expertise, specializing in <strong>Frontend development</strong> and with a strong foundation in <strong>Backend development.</strong> Here's a breakdown
+      Experienced Full Stack Engineer with 10 years of comprehensive expertise, specializing in <strong
+      >Frontend development</strong
+      >
+      and with a strong foundation in <strong>Backend development.</strong> Here's a breakdown
     </p>
   </div>
 </section>
-
 
 <AboutSection section={frontendSection} />
 <AboutSection section={backendSection} />
