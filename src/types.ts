c968c41ce48891e1690e0cd5df0861aac155c7fb
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-02-18 17:17:59 +0100

    feat: add about dynamic data setting

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,30 @@
 export interface WorkExperience {
-	role: string;
-	company: string;
-	type: string;
-	startDateMonth: string;
-	startDateYear: string;
-	endDateMonth?: string;
-	endDateYear?: string;
-	stillOpen: boolean;
-	description: string;
+  role: string;
+  company: string;
+  type: string;
+  startDateMonth: string;
+  startDateYear: string;
+  endDateMonth?: string;
+  endDateYear?: string;
+  stillOpen: boolean;
+  description: string;
 }
+
+interface Block {
+  title: string;
+  content: string;
+};
+
+export interface Section {
+  title: {
+    firstLine: string;
+    secondLine: string;
+  };
+  intro: {
+    title: string;
+    content: string; 
+  };
+  blocks: Block[];
+}
+
+
