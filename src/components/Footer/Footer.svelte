40faf6992f17266de5c30c69959e9139573289b7
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-02-28 03:15:24 +0100

    feat: add footer layout

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,1 +1,5 @@
-<div class="h-20">Footer here</div>
+<footer class="bg-neutral-900">
+  <div class="h-12 container mx-auto flex items-center">
+    <span class="font-robotoSlab font-extralight tracking-wide text-base text-white">&copy; {new Date().getFullYear()} Fabio Dessi</span>
+  </div>
+</footer>
