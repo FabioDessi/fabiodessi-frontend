c3f7114eef0982ee19aa22d502905e06f8820dac
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-03-01 23:06:45 +0100

    feat: menu overlay rework

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,10 +1,13 @@
-<script>
-	import BurgerMenu from '../../icons/BurgerMenu.svelte';
+<script lang="ts">
+import BurgerMenu from '../../icons/BurgerMenu.svelte';
+
+export let showMenu: boolean;
+export let toggleMenuOverlay: () => void;
 </script>
 
-<div class="h-100 py-1 px-10 flex justify-between items-center sticky top-0">
-	<a href="/">
-		<h1 class="font-integralCF font-extrabold text-2xl">FABIODESSI</h1>
-	</a>
-	<BurgerMenu />
+<div class="h-100 py-1 px-10 flex justify-between items-center sticky top-0 z-20">
+  <a href="/">
+    <h1 class="font-integralCF font-extrabold text-2xl">FABIODESSI</h1>
+  </a>
+  <BurgerMenu toggleMenuOverlay={toggleMenuOverlay} showMenu={showMenu} />
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,25 @@
-<script>
-	import '../app.css';
-	import MenuOverlay from '../components/MenuOverlay/MenuOverlay.svelte';
-	import Header from '../components/Header/Header.svelte';
-	import Footer from '../components/Footer/Footer.svelte';
-	import CustomCursor from '../components/CustomCursor/CustomCursor.svelte';
+<script lang="ts">
+import '../app.css';
+import MenuOverlay from '../components/MenuOverlay/MenuOverlay.svelte';
+import Header from '../components/Header/Header.svelte';
+import Footer from '../components/Footer/Footer.svelte';
+import CustomCursor from '../components/CustomCursor/CustomCursor.svelte';
+
+let showMenu = false;
+
+const toggleMenuOverlay = () => {
+  const body = document.body;
+  body?.classList.toggle('overflow-hidden');
+  showMenu = !showMenu;
+};
+
 </script>
 
-<MenuOverlay />
+<MenuOverlay showMenu={showMenu} toggleMenuOverlay={toggleMenuOverlay} />
 
 <div class="w-full">
-	<CustomCursor />
-	<Header />
-	<slot />
-	<Footer />
+  <CustomCursor />
+  <Header toggleMenuOverlay={toggleMenuOverlay} showMenu={showMenu} />
+  <slot />
+  <Footer />
 </div>
